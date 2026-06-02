3c33c535fc72839e59d5fc92efda8a24f6d9db9c
Amrmoo2004 <amrmohamad489@gmail.com>
2026-06-02 20:49:13 +0300

    Enhance community routes: add authentication middleware for post creation, deletion, like toggling, and comments

--- src/modules/community/community.routes.js
+++ src/modules/community/community.routes.js
@@ -1,19 +1,20 @@
 import { Router } from "express";
 import * as communityServices from './community.services.js';
+import { authenticateToken } from '../../middleware/auth.middleware.js';
 
 const router = Router();
 
 // Posts routes
-router.post("/posts", communityServices.createPost);
+router.post("/posts", authenticateToken, communityServices.createPost);
 router.get("/posts", communityServices.getPaginatedPosts);
 router.get("/posts/:id", communityServices.getPostById);
-router.delete("/posts/:id", communityServices.deletePost);
+router.delete("/posts/:id", authenticateToken, communityServices.deletePost);
 
 // Like toggle
-router.put("/posts/:id/like", communityServices.toggleLike);
+router.put("/posts/:id/like", authenticateToken, communityServices.toggleLike);
 
 // Comments routes
-router.post("/posts/:id/comments", communityServices.addComment);
+router.post("/posts/:id/comments", authenticateToken, communityServices.addComment);
 router.get("/posts/:id/comments", communityServices.getPostComments);
 
 export default router;
